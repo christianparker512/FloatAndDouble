e1ace1e5594eebee572ea4b36609a535e91e03dc
Christian Parker <christianparker512@gmail.com>
2021-09-01 12:50:02 -0500

    float and double

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -1,9 +1,19 @@
-package parker
-        .learningJava;
+package parker.learningJava;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //single precision 32 bits
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Min value = " + myMinFloatValue);
+        //double precision 64 bits
+        System.out.println("Float Max Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Min value = " + myMinDoubleValue);
+        //double precision 64 bits
+        System.out.println("Double Max Value = " + myMaxDoubleValue);
     }
 }
